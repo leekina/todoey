2ad95b96a91427cab59fb97421e8d4cd89dc66b3
leekina <leemh332@gmail.com>
2023-05-25 23:11:56 +0900

    CHORE : 실행 오류 const 제거

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'screens/tasks_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
